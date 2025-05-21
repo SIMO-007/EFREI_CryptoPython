eb54038f47d3db6a536369d75f94f66a0f514326
SIMO-007 <105717626+SIMO-007@users.noreply.github.com>
2025-05-21 08:47:33 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template request
+from flask import render_template, request
 from flask import json
 from urllib.request import urlopen
 import sqlite3
